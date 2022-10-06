dbe94f3e4ff55f30c8bcd22ccf863bd62ca0932c
Anderson Lomba <andersonlomba172@outlook.com>
2022-10-06 01:43:37 -0300

    Dados do banco

--- projects/tasks/models.py
+++ projects/tasks/models.py
@@ -9,11 +9,14 @@ class Task(models.Model):
     )
 
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    # zip_code = models.IntegerField()
+    # cost = models.IntegerField()
+    # description = models.TextField()
     done = models.CharField(
         max_length=5,
         choices=STATUS,
     )
+    # deadline = models.CharField(max_length= 150)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- projects/todo/settings.py
+++ projects/todo/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+     # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'tasks',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'tdbtdb',
+    #     'HOST': 'localhost'
+    # }
 }
 
 
